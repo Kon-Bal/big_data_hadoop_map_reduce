786874ab2be5e8ec5eb35794a304ead9e1d0a240
Kon-Bal <kbalacki@gmail.com>
2021-04-07 17:50:34 +0200

    -

--- 01_intro/01_simple_map_reduce.py
+++ 01_intro/01_simple_map_reduce.py
@@ -4,3 +4,11 @@ class MRWordCount(MRJob):
 
     def mapper(self, _, line):
         yield 'chars', len(line)
+        yield 'words', len(line.split())
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    MRWordCount.run()
